d634afe1a44c9129f750f8a07cd87a2cc8d87b31
mtgaffney <89404474+mtgaffney@users.noreply.github.com>
2021-10-05 22:44:21 -0400

    Adds error message if no date

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -10,7 +10,7 @@
 #--------------------------------------------------------------
 
 #Ask user for the search date
-user_date = input("Enter date to search for Sara: ")
+user_date = input("Enter date to search for Sara [M/D/YYYY]: ")
 
 #Create a variable pointing to data file
 file_name = './data/raw/sara.txt'
@@ -57,6 +57,9 @@ for date_item in date_dict.items():
     if the_date == user_date:
         #If so, add the key to the list
         matching_keys.append(the_key)
+#If no records are found tell the user
+if len(matching_keys) == 0:
+    print(f"No observations on {user_date}")
         
 #Reveal the locationms for eacgh key in the matching keys
 for matching_keys in matching_keys:
